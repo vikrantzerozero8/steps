a2303a0bfe6ab295095371756d5fdd463958d278
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2024-07-16 19:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ pdf_url = "https://arxiv.org/pdf/1910.13461.pdf" # also allowed is a file path e
 # pdf_url = "https://arxiv.org/pdf/2212.14024.pdf"
 pdf_reader = LayoutPDFReader(llmsherpa_api_url)
 doc = pdf_reader.read_pdf(pdf_url)
-print(doc) 
+doc
